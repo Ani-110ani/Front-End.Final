9c189d5212fc7eb5c487bfb24750e991c35c731d
Ani-110ani <ani6asatiani@gmail.com>
2026-09-09 15:41:51 +0400

    add project filtering in js

--- src/js/app.js
+++ src/js/app.js
@@ -22,4 +22,25 @@ document.addEventListener("DOMContentLoaded", () => {
 
   progressBars.forEach(bar => progressObserver.observe(bar));
 
+  const filterBtns = document.querySelectorAll(".filter-btn");
+  const projectCards = document.querySelectorAll(".project-card");
+
+  filterBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+      filterBtns.forEach(b => b.classList.remove("active"));
+      btn.classList.add("active");
+
+      const filterValue = btn.getAttribute("data-filter");
+      
+      projectCards.forEach(card => {
+        if (filterValue === "all" || card.getAttribute("data-category") === filterValue) {
+          card.classList.remove("hide");
+        } else {
+          card.classList.add("hide");
+        }
+      });
+    });
+  });
+
+
   
